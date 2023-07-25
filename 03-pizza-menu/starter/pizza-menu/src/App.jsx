a2ecfc3220adb829d963c9a49fe6183e4f520e1b
Eniola Bakare <eebakare@gmail.com>
2023-07-25 23:32:26 +0100

    Initial commit of complete react course resource.

---
+++ 03-pizza-menu/starter/pizza-menu/src/App.jsx
@@ -0,0 +1,15 @@
+import '../src/index.css'
+
+function App() {
+  return (
+    <div>
+      <p>Hello REact!</p>
+      <img src="pizzas/salamino.jpg" alt="pizza" />
+      <img src="pizzas/salamino.jpg" alt="pizza" />
+      <img src="pizzas/salamino.jpg" alt="pizza" />
+      <img src="pizzas/salamino.jpg" alt="pizza" />
+    </div>
+  );
+}
+
+export default App;

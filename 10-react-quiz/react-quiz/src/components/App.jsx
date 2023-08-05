6a616e0124c73d769e76c03892c97cec428d44bf
Eniola Bakare <eebakare@gmail.com>
2023-08-05 15:21:07 +0100

    Learning useReducer

---
+++ 10-react-quiz/react-quiz/src/components/App.jsx
@@ -0,0 +1,17 @@
+import DateCounter from "../components/DateCounter";
+import Header from "./Header";
+import Main from './Main'
+
+function App() {
+  return (
+    <div className="app">
+      {/* <DateCounter /> */}
+      <Header />
+      <Main>
+        <p>Hii</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;

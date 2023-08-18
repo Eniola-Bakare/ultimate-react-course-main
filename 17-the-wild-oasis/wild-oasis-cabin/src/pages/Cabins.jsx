b93269b21d9a5bce70817f30b2820a6df23bbd6e
Eniola Bakare <eebakare@gmail.com>
2023-08-19 00:19:16 +0100

    React Query: Fetching Data & Mutations

--- 17-the-wild-oasis/wild-oasis-cabin/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/wild-oasis-cabin/src/pages/Cabins.jsx
@@ -1,21 +1,21 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
   // useEffect(() => {
   //   getCabins().then((data) => console.log(data));
   // }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://dwwurjjcuqaotnysxatb.supabase.co/storage/v1/object/public/cabinImages/cabin-001.jpg"
-        alt=""
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter/Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
--- 17-the-wild-oasis/wild-oasis-cabin/src/services/apiCabins.js
+++ 17-the-wild-oasis/wild-oasis-cabin/src/services/apiCabins.js
@@ -10,3 +10,12 @@ export async function getCabins() {
 
   return data;
 }
+export async function deleteCabin( id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    throw new Error("Cabins cannot be deleted at the moment. Please Try again");
+  }
+
+  return data;
+}

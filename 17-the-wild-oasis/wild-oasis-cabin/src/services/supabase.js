9e821c42627ecfb1192d06add7c3671120b6f06c
Eniola Bakare <eebakare@gmail.com>
2023-08-18 03:03:39 +0100

    Setup: Supabase for backend remote state management

---
+++ 17-the-wild-oasis/wild-oasis-cabin/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://dwwurjjcuqaotnysxatb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3d3VyampjdXFhb3RueXN4YXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIzMTgwOTQsImV4cCI6MjAwNzg5NDA5NH0.Y9XN_2EndiL1ctzBIcc9DWo5dX85oT4ex70Hj_91L18";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

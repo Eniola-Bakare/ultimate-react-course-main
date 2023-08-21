cd61bb7a3e5c7e7ceae01c03eed8b61fe6e7b792
Eniola Bakare <eebakare@gmail.com>
2023-08-22 00:10:53 +0100

    Feature: using compound component for reusable table

--- 17-the-wild-oasis/wild-oasis-cabin/src/services/apiCabins.js
+++ 17-the-wild-oasis/wild-oasis-cabin/src/services/apiCabins.js
@@ -4,7 +4,6 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.log(error);
     throw new Error("Cabins cannot be loaded at the moment. Please Try again");
   }
 

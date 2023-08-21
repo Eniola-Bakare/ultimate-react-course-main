4a72eddc62d6eb6fb02919c251fbe1fec8563c00
Eniola Bakare <eebakare@gmail.com>
2023-08-21 19:46:43 +0100

    Refactor: modal to implement compound component pattern

--- 17-the-wild-oasis/wild-oasis-cabin/src/features/cabins/AddModal.jsx
+++ 17-the-wild-oasis/wild-oasis-cabin/src/features/cabins/AddModal.jsx
@@ -2,23 +2,45 @@ import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import { HiXMark } from "react-icons/hi2";
+import CabinTable from "./CabinTable";
 
 function AddModal() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((isOpenModal) => !isOpenModal)}>
-        Show
-      </Button>
-      {isOpenModal && (
-        <Modal
-          onCloseModal={() => setIsOpenModal((isOpenModal) => !isOpenModal)}
-        >
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add a new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddModal() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((isOpenModal) => !isOpenModal)}>
+//         Add new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal
+//           onCloseModal={() => setIsOpenModal((isOpenModal) => !isOpenModal)}
+//         >
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddModal;

e6da9c9b05f4f968cabab860c898412182f0dbab
Eniola Bakare <eebakare@gmail.com>
2023-08-30 16:03:43 +0100

    Page: Dashboard - activities

--- 17-the-wild-oasis/wild-oasis-cabin/src/features/check-in-out/CheckoutButton.jsx
+++ 17-the-wild-oasis/wild-oasis-cabin/src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,17 @@
+import { useCheckOut } from "./useCheckOut";
+
 import Button from "../../ui/Button";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingout } = useCheckOut();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      disabled={isCheckingout}
+      onClick={() => checkout(bookingId)}
+    >
       Check out
     </Button>
   );

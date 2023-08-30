6c4c815d8e6ef420caf171a5487660dfa3bd130b
Eniola Bakare <eebakare@gmail.com>
2023-08-30 12:48:33 +0100

    Page: dashboard stats

--- 17-the-wild-oasis/wild-oasis-cabin/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/wild-oasis-cabin/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -7,13 +12,28 @@ const StyledDashboardLayout = styled.div`
   gap: 2.4rem;
 `;
 
-
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  console.log(stays, bookings);
+  if (isLoading1 || isLoading2) return <Spinner />;
   return (
-   <StyledDashboardLayout>
-    <p>JJ</p>
-   </StyledDashboardLayout>
-  )
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+    </StyledDashboardLayout>
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
